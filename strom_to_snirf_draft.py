85483af8096a4a1f746dd3d91d9b46cca8e7e6cd
Nina_Besser_Ilan <nina.besser.ilan@gmail.com>
2023-07-19 13:59:34 +0300

    adding the sourcers and detectors data to the init

--- strom_to_snirf_draft.py
+++ strom_to_snirf_draft.py
@@ -13,6 +13,11 @@ class Niralysis:
         if not self.snirf_fname.exists():
             raise ValueError("File does not exsist.") 
         
+        self.read_snirf()
+        self.snirf_dtc_loc = self.snirf_file.nirs[0].probe.detectorPos3D[:,:]
+        self.snirf_src_loc = self.snirf_file.nirs[0].probe.sourcePos3D[:,:]
+
+        
     
     def read_snirf(self):
         self.snirf_file = Snirf(self.snirf_fname, 'r+')
@@ -22,16 +27,17 @@ class Niralysis:
 
 
     def is_same_dim(self):
-        snirf_dtc_loc = self.snirf_file.nirs[0].probe.detectorPos3D[:,:]
-        snirf_src_loc = self.snirf_file.nirs[0].probe.sourcePos3D[:,:]
-        num_snirf_src_loc = np.shape(snirf_src_loc)[0]
-        num_snirf_dtc_loc = np.shape(snirf_dtc_loc)[0]
+        num_snirf_src_loc = np.shape(self.snirf_src_loc)[0]
+        num_snirf_dtc_loc = np.shape(self.snirf_dtc_loc)[0]
         num_storm_dtc = 23
         num_storm_src = 16
 
         if num_snirf_src_loc !=num_storm_src or num_snirf_dtc_loc !=num_storm_dtc:
             raise ValueError("File does not exsist.")
+        
 
+
+    
 
 
 
