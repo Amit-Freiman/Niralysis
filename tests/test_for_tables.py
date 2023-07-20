7ef7f7856eec5b3b691ae5967d21429c7c9a1b94
NaamonetHa <naamaharari@mail.tau.ac.il>
2023-07-21 00:07:49 +0300

    Add test for events in test_for_table

--- tests/test_for_tables.py
+++ tests/test_for_tables.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import pytest
 from niralysis.niralysis import *
+from niralysis.Events_to_label import events_to_labels 
+
 # Define fixture
 
 @pytest.fixture
@@ -33,3 +35,18 @@ def test_calculate_change_in_position_per_frame(example_data, expected_change_in
     """
     change_in_position_table = Niralysis.calculate_change_in_position_per_frame(example_data)
     assert change_in_position_table[:5].equals(expected_change_in_position_per_frame)
+
+
+def test_dataframe_with_no_timestamp_column():
+    # Create a test DataFrame without the 'timestamp' column
+    data = {
+        'Col0': [1, 2, 3],
+        'Col1': [4, 5, 6],
+        'Col': [7, 8, 9]
+    }
+    df = pd.DataFrame(data)
+
+    # Ensure that the function raises a KeyError when 'timestamp' column is missing
+    with pytest.raises(KeyError):
+        events_to_labels(df) 
+
