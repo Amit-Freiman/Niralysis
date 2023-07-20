08b9fcbbee4a9ab9f387b4ad4aca65d1ae818701
misTBD <6tamar@gmail.com>
2023-07-20 12:22:42 +0300

    test2

--- tests/test_for_tables.py
+++ tests/test_for_tables.py
@@ -1,14 +1,14 @@
 import os
-import sys  # Add the missing import for sys
+import sys
 import pytest
 import csv
 from Niralysis_openpose import calculate_change_in_distance, calculate_change_in_position_per_frame
 
-# Get the parent directory (Niralysis) of the current script (test_for_tables.py)
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Append the parent directory to the Python path
-sys.path.append(parent_dir)
+# Define path names for test data
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'csv4test')
+EXAMPLE_HEAD_DATA_CSV = os.path.join(TEST_DATA_DIR, 'example_head_data.csv')
+TRUTH_TABLE_DISTANCE_DIFF_FRAMES_CSV = os.path.join(TEST_DATA_DIR, 'truth_table_distance_diff_frames.csv')
+TRUTH_DIFF_FRAMES_CSV = os.path.join(TEST_DATA_DIR, 'truth_diff_frames.csv')
 
 def read_csv(file_path):
     with open(file_path, 'r') as csvfile:
@@ -19,22 +19,18 @@ def read_csv(file_path):
 
 @pytest.fixture
 def expected_change_in_distance():
-    file_path = os.path.join(parent_dir, 'tests', 'csv4test', 'truth_table_distance_diff_frames.csv')
-    return read_csv(file_path)[:4]
+    return read_csv(TRUTH_TABLE_DISTANCE_DIFF_FRAMES_CSV)[:4]
 
 @pytest.fixture
 def expected_change_in_position_per_frame():
-    file_path = os.path.join(parent_dir, 'tests', 'csv4test', 'truth_diff_frames.csv')
-    return read_csv(file_path)[:5]
+    return read_csv(TRUTH_DIFF_FRAMES_CSV)[:5]
 
 def test_calculate_change_in_distance(expected_change_in_distance):
-    file_path = os.path.join(parent_dir, 'tests', 'csv4test', 'example_head_data.csv')
-    data = read_csv(file_path)
+    data = read_csv(EXAMPLE_HEAD_DATA_CSV)
     change_in_distance_table = calculate_change_in_distance(data)
     assert change_in_distance_table[:4] == expected_change_in_distance
 
 def test_calculate_change_in_position_per_frame(expected_change_in_position_per_frame):
-    file_path = os.path.join(parent_dir, 'tests', 'csv4test', 'example_head_data.csv')
-    data = read_csv(file_path)
+    data = read_csv(EXAMPLE_HEAD_DATA_CSV)
     change_in_position_table = calculate_change_in_position_per_frame(data)
     assert change_in_position_table[:5] == expected_change_in_position_per_frame
