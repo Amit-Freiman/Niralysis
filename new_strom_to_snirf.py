a9fde0a56eeaa46fd04277aa07a8fc249ded8dfb
Nina_Besser_Ilan <nina.besser.ilan@gmail.com>
2023-07-20 19:18:40 +0300

    finish the storm section minus documentation

--- new_strom_to_snirf.py
+++ new_strom_to_snirf.py
@@ -67,6 +67,7 @@ class Niralysis:
                 self.snirf_sourc_loc = data.nirs[0].probe.sourcePos3D[:,:]
                 self.set_storm_file(storm_fname)
                 self.snirf_with_storm_prob(data)
+
                 print("Your snirf file has been updated.")
                 a = input("If you want to see the updated sources and detectors locations, press 1. Else, press 0")
                 if a == str(1):
@@ -83,8 +84,6 @@ class Niralysis:
             print('We took care of it, please try again')
             
             
-
-
 
 
     def set_storm_file(self, storm_fname: str):
@@ -137,9 +136,28 @@ class Niralysis:
         if len(storm_sourc_loc) == 0 or len(storm_detc_loc) == 0:
             raise ValueError("Invalid STORM data. Check your STORM file.")
 
-
         return storm_sourc_loc, storm_detc_loc
     
+    ###validation STOM file###
+    def is_storm_valid(self):
+        storm_sourc_loc, storm_detc_loc = self.storm_prob()
+
+        if storm_sourc_loc.isnull().values.any() or storm_detc_loc.isnull().values.any():
+            raise ValueError("Invalid STORM data. Check your STORM file.")
+        
+        if any(storm_sourc_loc.dtypes == int or storm_detc_loc.dtypes ==int):
+            raise ValueError("STORM data contains integer column(s). All columns should have float data type.")
+ 
+        num_storm_sourc = np.shape(storm_sourc_loc)
+        num_storm_detc = np.shape(storm_detc_loc)
+        
+        num_snirf_sourc = np.shape(self.snirf_sourc_loc)[0]
+        num_snirf_detc = np.shape(self.snirf_detc_loc)[0]
+
+        if num_snirf_sourc != num_storm_sourc or num_snirf_detc != num_storm_detc:
+            raise ValueError("Invalid STORM data. Check your STORM file.")
+        
+
 
     def snirf_with_storm_prob(self,data):
         
@@ -160,6 +178,7 @@ class Niralysis:
         
         storm_sourc_loc, storm_detc_loc = self.storm_prob()
 
+
         # Store the old optode locations if not already stored
         if self.old_sourc_loc is None:
             self.old_sourc_loc = np.copy(data.nirs[0].probe.sourcePos3D)
@@ -171,18 +190,9 @@ class Niralysis:
         data.nirs[0].probe.detectorPos3D[:, :] = storm_detc_loc
 
         data.save(rf'{self.snirf_fname}')
+        data.close()
 
-
-    def is_same_dim(self):
-        num_snirf_sourc = np.shape(self.snirf_sourc_loc)[0]
-        num_snirf_detc = np.shape(self.snirf_detc_loc)[0]
-        storm_sourc_loc, storm_detc_loc = self.storm_prob()
-        num_storm_sourc = np.shape(storm_sourc_loc)
-        num_storm_detc = np.shape(storm_detc_loc)
-        if num_snirf_sourc != num_storm_sourc or num_snirf_detc != num_storm_detc:
-            raise ValueError("File does not exist.")
-
-
+        
     def get_old_probe_locations(self):
         """    
     Get the original probe locations before updating with STORM data.
@@ -191,6 +201,30 @@ class Niralysis:
         Tuple[numpy.ndarray or None, numpy.ndarray or None]: A tuple containing the original source and detector optode locations."""
         return self.old_sourc_loc, self.old_detc_loc
     
+
+    def euclidean_dist(self):
+        self.old_sourc_loc, self.old_detc_loc = self.get_old_probe_locations()
+        storm_sourc_loc, storm_detc_loc = self.storm_prob()
+
+        sourc_euclidean_dist = np.linalg.norm(storm_sourc_loc - self.old_sourc_loc, axis=1)
+        detc_euclidean_dist = np.linalg.norm(storm_detc_loc - self.old_detc_loc, axis=1)
+
+        return sourc_euclidean_dist, detc_euclidean_dist
+
+    
+    def invalid_sourc(self,thresh=10):
+        storm_sourc_loc = self.storm_prob()[0]
+        sourc_euclidean_dist = self.euclidean_dist()[0]
+        sourc_euclidean_dist = np.array(sourc_euclidean_dist)
+        invalid_sourc = storm_sourc_loc[sourc_euclidean_dist >= thresh]
+        return invalid_sourc
+    
+    def invalid_detec(self,thresh=10):
+        storm_detc_loc = self.storm_prob()[1]
+        detc_euclidean_dist = self.euclidean_dist()[1]
+        detc_euclidean_dist = np.array(detc_euclidean_dist)
+        invalid_detec = storm_detc_loc[detc_euclidean_dist >= thresh]
+        return invalid_detec
 
 
 
