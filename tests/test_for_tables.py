0ba944a36247f2ba246fff3c0484f8e5ce213c67
misTBD <6tamar@gmail.com>
2023-07-20 20:16:21 +0300

    update

--- tests/test_for_tables.py
+++ tests/test_for_tables.py
@@ -1,29 +1,38 @@
+# test_niralysis.py
+
 import pandas as pd
 import pytest
 from niralysis.niralysis import Niralysis
+
+# Define fixtures
+
+@pytest.fixture
+def example_data():
+    # Load example data from a CSV file or create a DataFrame here for testing
+    return pd.read_csv('example_head_data.csv')
+
+@pytest.fixture
+def expected_change_in_position_per_frame():
+    # Load expected change in position per frame data from a CSV file or create a DataFrame here for testing
+    return pd.read_csv('truth_diff_frames.csv')
+
+@pytest.fixture
+def expected_change_in_distance():
+    # Load expected change in position per frame data from a CSV file or create a DataFrame here for testing
+    return pd.read_csv('truth_table_distance_diff_frames.csv')
+
 # Test functions
-def test_calculate_change_in_distance():
+
+def test_calculate_change_in_distance(example_data, expected_change_in_distance):
     """
     Test the calculate_change_in_distance function.
-
-    This test case reads data from example_data, calculates the change in distance
-    using the calculate_change_in_distance function, and compares the result with the
-    expected_change_in_distance fixture.
     """
-    example_data=pd.read_csv('example_head_data.csv')
-    expected_change_in_distance=pd.read_csv('truth_table_distance_diff_frames.csv')
     change_in_distance_table = Niralysis.calculate_change_in_distance(example_data)
     assert change_in_distance_table[:4].equals(expected_change_in_distance)
 
 def test_calculate_change_in_position_per_frame(example_data, expected_change_in_position_per_frame):
     """
     Test the calculate_change_in_position_per_frame function.
-
-    This test case reads data from example_data, calculates the change in position per frame
-    using the calculate_change_in_position_per_frame function, and compares the result with the
-    expected_change_in_position_per_frame fixture.
     """
-    example_data=pd.read_csv('example_head_data.csv')
-    expected_change_in_position_per_frame=pd.read_csv('truth_diff_frames.csv')
     change_in_position_table = Niralysis.calculate_change_in_position_per_frame(example_data)
     assert change_in_position_table[:5].equals(expected_change_in_position_per_frame)
