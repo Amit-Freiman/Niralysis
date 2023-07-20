3b6bba5d9dafdf842c24f89b389f4c12d3846e42
misTBD <6tamar@gmail.com>
2023-07-20 13:11:32 +0300

    readme

--- calculate_pairwise_distance.py
+++ calculate_pairwise_distance.py
@@ -2,32 +2,52 @@ import pandas as pd
 import numpy as np
 
 def calculate_pairwise_distance(data):
+    """
+    Calculate the pairwise distance between keypoints in each frame.
+
+    This function takes a DataFrame containing keypoint coordinates and computes the pairwise distance
+    between keypoints for each frame. The resulting distance data is returned as a DataFrame.
+
+    Parameters:
+        data (pd.DataFrame): DataFrame containing keypoint coordinates.
+            The DataFrame should have columns for 'frame', 'person', and keypoints in the format 'KP_i_x' and 'KP_i_y',
+            where 'i' represents the keypoint index.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the pairwise distance data for each frame.
+            The column names represent the pairs of keypoints for which the distance is calculated.
+            The row index represents the frame number.
+    """
     keypoints = [col for col in data.columns if col.endswith('_x')]
     num_keypoints = len(keypoints)
     num_frames = len(data)
     column_names = [f'{keypoints[i].replace("_x", "")}_{keypoints[j].replace("_x", "")}' for i in range(num_keypoints) for j in range(i+1, num_keypoints)]
 
+    # Create an empty DataFrame to store the pairwise distance data
     distance_data = pd.DataFrame(columns=column_names, index=range(num_frames))
 
+    # Loop through each frame in the input data
     for frame in range(num_frames):
+        # Loop through each keypoint
         for i in range(num_keypoints):
+            # Get the x and y coordinates of the first keypoint (KP_i_x and KP_i_y)
             x1, y1 = data[keypoints[i]].iloc[frame], data[keypoints[i].replace('_x', '_y')].iloc[frame]
+            # Loop through the rest of the keypoints to calculate distances
             for j in range(i + 1, num_keypoints):
+                # Get the x and y coordinates of the second keypoint (KP_j_x and KP_j_y)
                 x2, y2 = data[keypoints[j]].iloc[frame], data[keypoints[j].replace('_x', '_y')].iloc[frame]
 
                 # Check if any of the keypoint values are zero
                 if x1 == 0 or x2 == 0 or y1 == 0 or y2 == 0:
+                    # If any of the keypoints have a zero value, the distance is set to zero
                     distance = 0
                 else:
+                    # Calculate the Euclidean distance between the two keypoints
                     distance = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
+                # Store the distance in the corresponding cell of the DataFrame
                 col_name = f'{keypoints[i].replace("_x", "")}_{keypoints[j].replace("_x", "")}'
                 distance_data.at[frame, col_name] = distance
 
+    # Return the DataFrame containing the pairwise distance data
     return distance_data
-
-# Assuming "data" is the DataFrame containing the keypoint coordinates with columns like 'frame', 'person', 'KP_0_x', 'KP_0_y', 'KP_1_x', 'KP_1_y', ..., 'KP_n_x', 'KP_n_y'
-# Replace n with the total number of keypoints
-
-# Call the function to calculate the pairwise distance and get the resulting DataFrame
-#distance_table = calculate_pairwise_distance(data)
--- tests/test_for_tables.py
+++ tests/test_for_tables.py
@@ -11,6 +11,9 @@ TRUTH_TABLE_DISTANCE_DIFF_FRAMES_CSV = os.path.join(TEST_DATA_DIR, 'truth_table_
 TRUTH_DIFF_FRAMES_CSV = os.path.join(TEST_DATA_DIR, 'truth_diff_frames.csv')
 
 def read_csv(file_path):
+    """
+    Read data from a CSV file and convert it to a list of lists with float values.
+    """
     with open(file_path, 'r') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)  # Skip the header
@@ -19,18 +22,38 @@ def read_csv(file_path):
 
 @pytest.fixture
 def expected_change_in_distance():
+    """
+    Fixture to provide expected change in distance data for the test case.
+    """
     return read_csv(TRUTH_TABLE_DISTANCE_DIFF_FRAMES_CSV)[:4]
 
 @pytest.fixture
 def expected_change_in_position_per_frame():
+    """
+    Fixture to provide expected change in position per frame data for the test case.
+    """
     return read_csv(TRUTH_DIFF_FRAMES_CSV)[:5]
 
 def test_calculate_change_in_distance(expected_change_in_distance):
+    """
+    Test the calculate_change_in_distance function.
+
+    This test case reads data from EXAMPLE_HEAD_DATA_CSV, calculates the change in distance
+    using the calculate_change_in_distance function, and compares the result with the
+    expected_change_in_distance fixture.
+    """
     data = read_csv(EXAMPLE_HEAD_DATA_CSV)
-    change_in_distance_table = calculate_change_in_distance(data)
+    change_in_distance_table = calculate_change_in_distance.calculate_change_in_distance(data)
     assert change_in_distance_table[:4] == expected_change_in_distance
 
 def test_calculate_change_in_position_per_frame(expected_change_in_position_per_frame):
+    """
+    Test the calculate_change_in_position_per_frame function.
+
+    This test case reads data from EXAMPLE_HEAD_DATA_CSV, calculates the change in position per frame
+    using the calculate_change_in_position_per_frame function, and compares the result with the
+    expected_change_in_position_per_frame fixture.
+    """
     data = read_csv(EXAMPLE_HEAD_DATA_CSV)
-    change_in_position_table = calculate_change_in_position_per_frame(data)
+    change_in_position_table = calculate_change_in_position_per_frame.calculate_change_in_position_per_frame(data)
     assert change_in_position_table[:5] == expected_change_in_position_per_frame
