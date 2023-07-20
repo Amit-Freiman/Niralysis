d41e542d60ec561fefa02ffb8e220ac776c7c822
Amit-Freiman <amitfreiman@mail.tau.ac.il>
2023-07-20 22:09:43 +0300

    Fixed Events_to_label

--- niralysis/Events_to_label.py
+++ niralysis/Events_to_label.py
@@ -16,24 +16,24 @@ def events_to_labels (df: pd.DataFrame, T_Hold: int) -> pd.DataFrame:
     # Iterate over rows in the df and update values in the df_new 
     for index, row in df.iterrows():
         # Check if the movement is RIGHT TILT or LEAN
-        if (row['KP_0_x'] < 0 ) or (row['K_15_y'] > 0) or (row['K_17_y']>0):
+        if (row['KP_0_x'] < 0 ) or (row['KP_15_y'] > 0) or (row['KP_17_y']>0):
             if (row['KP_5_KP_16']> T_Hold) or (row['KP_2_KP_15']< -T_Hold): # negative value mean the keypoins got closer
                 df_new['TILT_R'] = True
             else: 
                 df_new['LEAN_R'] = True
        
         # Check if the movement is LEFT TILT or LEAN
-        if (row['KP_0_x'] > 0 ) or (row['K_16_y'] > 0) or (row['K_18_y'] > 0):
+        if (row['KP_0_x'] > 0 ) or (row['KP_16_y'] > 0) or (row['KP_18_y'] > 0):
             if (row['KP_5_KP_16']< -T_Hold) or (row['KP_2_KP_15'] > T_Hold):
                 df_new['TILT_L'] = True
             else: 
                 df_new['LEAN_L']= True
 
         # Check if movement is ROUND LEFT/RIGHT #
-        if row['KP_02_y']< -T_Hold:
-            if (row['KP_0_x'] < 0 ) or (row['K_16_x'] <0) or (row['K_18_x']<0):
+        if row['KP_2_y']< -T_Hold:
+            if (row['KP_0_x'] < 0 ) or (row['KP_16_x'] <0) or (row['KP_18_x']<0):
                 df_new['ROUND_R'] = True 
-        if row['KP_05_y']< -T_Hold:
+        if row['KP_5_y']< -T_Hold:
             if (row['KP_0_x'] > 0) or (row['KP_15_x'] > 0) or (row['KP_17_x'] > 0):
                 df_new['ROUND_L'] = True 
 
