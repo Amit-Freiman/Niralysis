53c4d709087de39fe61d25a9a6b70a8f1a8ba6c7
Michal Avidar <michal.avidar@mail.huji.ac.il>
2024-03-10 15:16:50 +0200

    "areas""

--- niralysis/SharedReality/consts.py
+++ niralysis/SharedReality/consts.py
@@ -1,4 +1,4 @@
-old_area_dict = {
+old_all_sizes = {
     "left TPJ": ["S8_D8", "S9_D8", "S10_D8"],
     "right TPJ": ["S14_D10", "S15_D10", "S16_D10", "S17_D10"],
 
@@ -8,4 +8,33 @@ old_area_dict = {
 
     "Auditory Cortex": ["S12_D6", "S7_D6", "S13_D7", "S7_D7"],
     "Occipital Cortex": ["S18_D11", "S19_D11", "S20_D11", "S21_D11"]
+}
+
+new_medium = {
+    "left TPJ": ["S16_D11", "S16_D13", "S16_D12", "S16_D14"],
+    "right TPJ": ["S1_D1", "S2_D1", "S3_D1"],
+
+    "left lPFC": ["S12_D9", "S9_D7", "S9_D8", "S10_D8"],
+    "left mPFC": ["S9_D5", "S9_D6", "S10_D6", "S8_D5"],
+    "right mPFC": ["S6_D5", "S6_D6", "S7_D7" "S5_D5"],
+
+    "Auditory Cortex": ["S12_D9", "S12_D10", "S15_D13", "S16_D13", "S14_D11", "S14_D13", "S12_D10", "S14_D10"],
+    "Somatosensory Cortex": ["S12_D11", "S13_D12", "S13_D11"],
+    "Occipital Cortex": ["S18_D15", "S20_D15", "S17_D15", "S19_D15", "S18_D16", "S20_D16"]
+}
+
+new_small = {
+    "left TPJ": ["S16_D12", "S16_D14", "S16_D11"],
+    "right TPJ": ["S2_D1", "S_D1", "S1_D1"],
+
+    "left lPFC": ["S12_D9", "S9_D7", "S9_D8", "S10_D8", "S12-D10"],
+    "left mPFC": ["S9_D5", "S9_D6", "S10_D6", "S8_D5"],
+    "right mPFC": ["S6_D5", "S6_D6", "S7_D7" "S5_D5"],
+
+    "Auditory Cortex": ["S12_D9", "S13_D13", "S14_D13", "S16_D13", "S14_D13", "S14_D10", "S15_D13"],
+    "Somatosensory Cortex": ["S12_D11", "S14_D11", "S13_D12", "S13_D11"],
+    "Occipital Cortex": ["S18_D15", "S20_D15", "S17_D15", "S19_D15", "S18_D16", "S20_D16"]
+}
+
+new_large = {
 }
