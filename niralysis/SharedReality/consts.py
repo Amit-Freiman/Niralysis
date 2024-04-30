95a5cae975e8929d4009cfd8ec40d865c58a0b3b
Amit-Freiman <amitfreiman@mail.tau.ac.il>
2024-04-30 14:26:47 +0300

    Changed and verified template dictionaries

--- niralysis/SharedReality/consts.py
+++ niralysis/SharedReality/consts.py
@@ -2,41 +2,83 @@ old_area_dict = {
     "left TPJ": ["S8_D8", "S9_D8", "S10_D8"],
     "right TPJ": ["S14_D10", "S15_D10", "S16_D10", "S17_D10"],
 
-    "left lPFC": ["S6_D4", "S5_D5", "S12_D5", "S3_D3", "S3_D4"],
-    "left mPFC": ["S3_D1", "S3_D2", "S4_D2"],
-    "right mPFC": ["S1_D1", "S1_D2", "S2_D2"],
+    "left vlPFC": ["S6_D4", "S5_D5", "S12_D5"],
+    "left dlPFC": ["S3_D3", "S3_D4"],
+    "left dmPFC": ["S3_D1", "S3_D2"],
+    "left vmPFC": ["S4_D2"],
+    "right vlPFC": [],
+    "right dlPFC": [],
+    "right dmPFC": ["S1_D1", "S1_D2"],
+    "right vmPFC": ["S2_D2"],
 
-    "Auditory Cortex": ["S12_D6", "S7_D6", "S13_D7", "S7_D7"],
-    "Occipital Cortex": ["S18_D11", "S19_D11", "S20_D11", "S21_D11"]
+    "Primary Auditory Cortex": ["S7_D7"],
+    "Secondary Auditory Cortex": ["S7_D6"],
+    "Broca": ["S12_D6", "S7_D6"],
+    "Wernicke": ["S13_D7"],
+    "Occipital Cortex (V1)": ["S18_D11", "S19_D11", "S20_D11", "S21_D11"] 
 }
 
 new_medium = {
-    "left TPJ": ["S16_D11", "S16_D13", "S16_D12", "S16_D14"],
+    "left TPJ": ["S16_D13", "S15_D13"],
     "right TPJ": ["S1_D1", "S2_D1", "S3_D1"],
 
-    "left lPFC": ["S12_D9", "S9_D7", "S9_D8", "S10_D8"],
-    "left mPFC": ["S9_D5", "S9_D6", "S10_D6", "S8_D5"],
-    "right mPFC": ["S6_D5", "S6_D6", "S7_D7" "S5_D5"],
+    "left vlPFC": ["S12_D9"],
+    "left dlPFC": ["S9_D7", "S9_D8", "S10_D8"],
+    "left dmPFC": ["S9_D5", "S9_D6", "S10_D6"],
+    "left vmPFC": ["S8_D5"],
+    "right dlPFC": ["S6_D3", "S6_D4", "S7_D4"],
+    "right vlPFC": ["S4_D3", "S4_D4"],
+    "right dmPFC": ["S6_D5", "S6_D6", "S7_D6"],    
+    "right vmPFC": ["S5_D5"],
 
-    "Auditory Cortex": ["S12_D9", "S12_D10", "S15_D13", "S16_D13", "S14_D11", "S14_D13", "S12_D10", "S14_D10"],
-    "Somatosensory Cortex": ["S12_D11", "S13_D12", "S13_D11"],
-    "Occipital Cortex": ["S18_D15", "S20_D15", "S17_D15", "S19_D15", "S18_D16", "S20_D16"]
+    "Primary Auditory Cortex": ["S14_D11"],
+    "Secondary Auditory Cortex": ["S12_D10", "S14_D10", "S14_D13"],
+    "Broca": ["S12_D9", "S12_D10"],
+    "Wernicke": ["S15_D13", "S16_D13"],
+    "Occipital Cortex (V1)": ["S18_D16", "S20_D16", "S18_D15", "S20_D15"],
+    "Somatosensory Cortex (M1/S1)": ["S12_D11", "S13_D12", "S13_D11"] 
 }
 
 new_small = {
-    "left TPJ": ["S16_D12", "S16_D14", "S16_D11"],
-    "right TPJ": ["S2_D1", "S3_D1", "S1_D1"],
+    "left TPJ": ["S16_D13", "S16_D14"],
+    "right TPJ": ["S2_D1", "S3_D1"],
 
-    "left lPFC": ["S12_D9", "S9_D7", "S9_D8", "S10_D8", "S12-D10"],
-    "left mPFC": ["S9_D5", "S9_D6", "S10_D6", "S8_D5"],
-    "right mPFC": ["S6_D5", "S6_D6", "S7_D7" "S5_D5"],
+    "left vlPFC": ["S12_D9", "S12_D10"],
+    "left dlPFC": ["S9_D7", "S9_D8", "S10_D8"],
+    "left dmPFC": ["S9_D5", "S9_D6", "S10_D6"],
+    "left vmPFC": ["S8_D5"],
+    "right dlPFC": ["S6_D3", "S6_D4", "S7_D4"],
+    "right vlPFC": ["S4_D3", "S4_D4"],
+    "right dmPFC": ["S6_D5", "S6_D6", "S7_D6"],    
+    "right vmPFC": ["S5_D5"],
 
-    "Auditory Cortex": ["S12_D9", "S13_D13", "S14_D13", "S16_D13", "S14_D13", "S14_D10", "S15_D13"],
-    "Somatosensory Cortex": ["S12_D11", "S14_D11", "S13_D12", "S13_D11"],
-    "Occipital Cortex": ["S18_D15", "S20_D15", "S17_D15", "S19_D15", "S18_D16", "S20_D16"]
+    "Primary Auditory Cortex": ["S14_D13", "S16_D13"],
+    "Secondary Auditory Cortex": ["S14_D10"],
+    "Broca": ["S12_D9"],
+    "Wernicke": ["S16_D13", "S15_D13"],
+    "Occipital Cortex (V1)": ["S18_D16", "S20_D16", "S18_D15", "S20_D15"],
+    "Somatosensory Cortex (M1/S1)": ["S12_D11", "S14_D11", "S13_D12", "S13_D11"] 
 }
 
 new_large = {
+    "left TPJ": ["S16_D11","S16_D13", "S15_D13"],
+    "right TPJ": ["S1_D1", "S2_D1"],
+
+    "left vlPFC": [],
+    "left dlPFC": ["S8_D7", "S8_D8", "S9_D8"],
+    "left dmPFC": ["S9_D5", "S9_D6", "S10_D6", "S8_D5"],
+    "left vmPFC": [],
+    "right dlPFC": ["S5_D3", "S5_D4", "S6_D4"],
+    "right vlPFC": ["S4_D3", "S4_D4"],
+    "right dmPFC": ["S6_D5", "S6_D6", "S7_D6", "S5_D5"],    
+    "right vmPFC": [],
+
+    "Primary Auditory Cortex": ["S14_D11"],
+    "Secondary Auditory Cortex": ["S14_D10", "S14_D13"],
+    "Broca": ["S12_D9", "S12_D10"],
+    "Wernicke": ["S15_D13", "S16_D13"],
+    "Occipital Cortex (V1)": ["S17_D15", "S18_D15", "S19_D15", "S20_D15"],
+    "Somatosensory Cortex (M1/S1)": ["S12_D11", "S13_D12", "S13_D11"] 
 }
 
 def templates_handler(date):
@@ -64,7 +106,7 @@ CONTROL_1 = "control first watch"
 CONTROL_2 = "control second watch"
 TABLE_ROWS = [A_PRE_CHOICE_1, A_PRE_CHOICE_2, B_PRE_CHOICE_1, B_PRE_CHOICE_2, POST_CHOICE_1, POST_CHOICE_2, CONTROL_1,
               CONTROL_2]
-UNIQUE_COMBINATION = "S1_D1"
+UNIQUE_COMBINATION = "S8_D5"
 
 EVENTS_TABLE_NAMES = ["Yael", "Roy", "Sahar", "Alon", "discussion:A", "discussion:B", "open discussion", "Yael", "Roy",
                       "Sahar",
