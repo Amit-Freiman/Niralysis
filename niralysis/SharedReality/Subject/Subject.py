1d6ea7a27a0888ecd156d4a00b37fe25949c70c6
Amit-Freiman <amitfreiman@mail.tau.ac.il>
2024-05-02 18:34:06 +0300

    Added steps for preprocessing by events

--- niralysis/SharedReality/Subject/Subject.py
+++ niralysis/SharedReality/Subject/Subject.py
@@ -1,10 +1,12 @@
 from niralysis.Niralysis import Niralysis
 from niralysis.SharedReality.consts import *
 from niralysis.utils.consts import *
-
+from niralysis.HbOData.HbOData import HbOData
+import pandas as pd
+import mne
 
 class Subject:
-    def __init__(self, path: str, areas: dict = None):
+    def __init__(self, path: str, areas: dict = None, preprocess_by_events: bool = False):
         if not path:
             self.events_data = None
         else:
@@ -13,10 +15,14 @@ class Subject:
             self.subject = Niralysis(path)
             self.subject.events_handler.set_continuous_events_frame()
             self.events_table = self.subject.events_handler.get_continuous_events_frame()
-            
-            self.subject.set_hbo_data(None, False, high_pass_freq=0.4)
             self.events_data = None
-            self.data_by_ares = False
+            self.data_by_ares = False  
+            self.preprocess_by_events = preprocess_by_events          
+            if not  self.preprocess_by_events:
+                self.subject.set_full_hbo_data(None, False, high_pass_freq=0.4)
+            else:
+                self.subject.hbo_data.raw_data = 0
+
             if areas is not None:
                 self.subject.hbo_data.set_data_by_areas(areas)
                 self.data_by_ares = True
@@ -33,13 +39,18 @@ class Subject:
     def set_events_data(self):
         events_data = {FIRST_WATCH: {}, DISCUSSIONS: {}, SECOND_WATCH: {}}
         for index, event in self.events_table.iterrows():
-            events_data[EVENTS_CATEGORY[index]][event[EVENT_COLUMN]] = self.get_event_data(event)
+            events_data[EVENTS_CATEGORY[index]][event[EVENT_COLUMN]] = self.get_event_data(event, self.preprocess_by_events)
         self.events_data = events_data
 
-    def get_event_data(self, event):
-        data = self.get_hbo_data()
-        event_data = data[(data[TIME_COLUMN] >= event[START_COLUMN]) & (data[TIME_COLUMN] <= event[END_COLUMN])]
-        event_data.reset_index(drop=True, inplace=True)
+    def get_event_data(self, event, process_by_events=False):
+        if not process_by_events:
+            data = self.get_hbo_data()
+            event_data = data[(data[TIME_COLUMN] >= event[START_COLUMN]) & (data[TIME_COLUMN] <= event[END_COLUMN])]
+            event_data.reset_index(drop=True, inplace=True)
+        else:
+            raw_data = mne.io.read_raw_snirf(self.path, preload=True)
+            raw_data.crop(event[START_COLUMN], event[END_COLUMN])
+            event_data = HbOData.preprocess_by_event(raw_data, channels= None)
         return event_data
 
     def get_event_data_table(self, index, name):
